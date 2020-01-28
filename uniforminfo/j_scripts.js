dbf580aa234aa5c1bb404fb9b66f242784cb65fa
FutureFightFlair <45393711+FutureFightFlair@users.noreply.github.com>
2020-01-27 17:45:35 -0800

    faster

--- uniforminfo/j_scripts.js
+++ uniforminfo/j_scripts.js
@@ -271,28 +271,6 @@ flair.loadChoices = function () {
 
 			enter.appendChild(flair_choice);
 		}
-		
-filterSelection("all")
-  function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("flair flair-choice");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-		
-filterSelection1("all")
-		  function filterSelection1(c) {
-  var x, i;
-  x = document.getElementsByClassName("flair flair-choice");
-  if (c == "all") c = "uniform";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "type");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "type");
-  }
-}
 
 
 	}
@@ -501,6 +479,29 @@ n.show = function (o) {
 	}
 }
 
+
+filterSelection("all")
+  function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("flair flair-choice");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+		
+filterSelection1("all")
+		  function filterSelection1(c) {
+  var x, i;
+  x = document.getElementsByClassName("flair flair-choice");
+  if (c == "all") c = "uniform";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "type");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "type");
+  }
+}
+
 function filterSelection1(c) {
   var x, i;
   x = document.getElementsByClassName("flair flair-choice");
@@ -543,17 +544,6 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-function w3RemoveClass1(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
 
 	  $(function(){
 
